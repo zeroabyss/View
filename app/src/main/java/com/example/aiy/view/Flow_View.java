26386cf07b63a2a202c452c1938ed3f16244eba4
zero <422359126@qq.com>
2017-08-31 19:38:26 +0800

    ver0.9

--- app/src/main/java/com/example/aiy/view/Flow_View.java
+++ app/src/main/java/com/example/aiy/view/Flow_View.java
@@ -2,6 +2,7 @@ package com.example.aiy.view;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.view.View;
 import android.view.ViewGroup;
 
 /**
@@ -19,12 +20,10 @@ public class Flow_View extends ViewGroup {
     public LayoutParams generateLayoutParams(AttributeSet attrs) {
         return new MarginLayoutParams(getContext(),attrs);
     }
-
     @Override
     protected LayoutParams generateLayoutParams(LayoutParams p) {
         return new MarginLayoutParams(p);
     }
-
     @Override
     protected LayoutParams generateDefaultLayoutParams() {
         return new MarginLayoutParams(LayoutParams.MATCH_PARENT,LayoutParams.MATCH_PARENT);
@@ -33,11 +32,68 @@ public class Flow_View extends ViewGroup {
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        int widthMode=MeasureSpec.getMode(widthMeasureSpec);
+        int widthSize=MeasureSpec.getSize(widthMeasureSpec);
+        int heightMode=MeasureSpec.getMode(heightMeasureSpec);
+        int heightSize=MeasureSpec.getSize(heightMeasureSpec);
 
+        int lineWidth=0;
+        int lineHeight=0;
+        int layoutWidth=0;
+        int layoutHeight=0;
+
+        for (int i=0;i<getChildCount();i++){
+            View child=getChildAt(i);
+            measureChildWithMargins(child,widthMeasureSpec,0,heightMeasureSpec,0);
+            MarginLayoutParams layoutParams= (MarginLayoutParams) child.getLayoutParams();
+            int childWidth=child.getMeasuredWidth()+layoutParams.leftMargin+layoutParams.rightMargin;
+            int childHeight=child.getMeasuredHeight()+layoutParams.topMargin+layoutParams.bottomMargin;
+            if (lineWidth+childWidth > widthSize){
+                layoutWidth=Math.max(lineWidth,childWidth);
+                layoutHeight+=lineHeight;
+                lineWidth=childWidth;
+                lineHeight=childHeight;
+            }else {
+                lineWidth+=childWidth;
+                lineHeight=Math.max(lineHeight,childHeight);
+            }
+            if (i==(getChildCount()-1)){
+                layoutHeight+=lineHeight;
+                layoutWidth=Math.max(layoutWidth,lineWidth);
+            }
+        }
+
+        setMeasuredDimension((widthMode==MeasureSpec.EXACTLY)?widthSize:layoutWidth,
+                (heightMode==MeasureSpec.EXACTLY)?heightSize:layoutHeight);
     }
 
     @Override
     protected void onLayout(boolean changed, int l, int t, int r, int b) {
+        int top=0;
+        int left=0;
+        int lineWidth=0;
+        int lineHeight=0;
 
+        for (int i=0;i<getChildCount();i++){
+            View child=getChildAt(i);
+            MarginLayoutParams lp= (MarginLayoutParams) child.getLayoutParams();
+            int childHeight=lp.topMargin+lp.bottomMargin+child.getMeasuredHeight();
+            int childWidth=lp.leftMargin+lp.rightMargin+child.getMeasuredWidth();
+            if (childWidth+lineWidth>getMeasuredWidth()){
+                left=0;
+                top+=lineHeight;
+                lineHeight=childHeight;
+                lineWidth=childWidth;
+            }else{
+                lineWidth+=childWidth;
+                lineHeight=Math.max(lineHeight,childHeight);
+            }
+            int leftPoint=left+lp.leftMargin;
+            int topPoint=top+lp.topMargin;
+            int rightPoint=leftPoint+child.getMeasuredWidth();
+            int bottomPoint=topPoint+child.getMeasuredHeight();
+            child.layout(leftPoint,topPoint,rightPoint,bottomPoint);
+            left+=childWidth;
+        }
     }
 }
